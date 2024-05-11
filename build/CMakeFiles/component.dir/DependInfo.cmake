3b0a04080dbb8f0d35745f4f7d8cb34e2f4cb3e4
danielbrambila <160786554+danielbrambila@users.noreply.github.com>
2024-05-11 10:50:39 +0000

    version1

---
+++ build/CMakeFiles/component.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/animation.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/animation.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/button.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/button.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/catch_event.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/catch_event.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/checkbox.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/checkbox.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/collapsible.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/collapsible.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/component.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/component.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/component_options.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/component_options.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/container.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/container.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/dropdown.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/dropdown.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/event.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/event.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/hoverable.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/hoverable.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/input.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/input.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/loop.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/loop.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/maybe.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/maybe.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/menu.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/menu.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/modal.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/modal.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/radiobox.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/radiobox.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/renderer.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/renderer.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/resizable_split.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/resizable_split.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/screen_interactive.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/screen_interactive.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/slider.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/slider.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/terminal_input_parser.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/terminal_input_parser.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/util.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/util.cpp.o"
+  "/workspaces/arkanoid/ftxui/src/ftxui/component/window.cpp" "/workspaces/arkanoid/build/CMakeFiles/component.dir/src/ftxui/component/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspaces/arkanoid/ftxui/src"
+  "/workspaces/arkanoid/ftxui/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspaces/arkanoid/build/CMakeFiles/dom.dir/DependInfo.cmake"
+  "/workspaces/arkanoid/build/CMakeFiles/screen.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ include/pintor/Dibujo.hpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <list>
+#include <Vector.hpp>
+using namespace std;
+
+class Dibujo
+{
+private:
+    list<string> lineas;
+    Vector posicion;
+    Vector velocidad;
+public:
+    Dibujo() {}
+    ~Dibujo() {}
+    void AgregarLinea(string linea){
+        this->lineas.push_back(linea);
+    }
+    list<string> ObtenerLineas(){
+        return this->lineas;
+    }
+};
